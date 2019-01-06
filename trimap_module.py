7fa374c176c87abb3d7e89f9324163f2062fb297
Leo <32232486+lnugraha@users.noreply.github.com>
2019-01-06 22:22:12 +0800

    Include erosion function inside the trimap_generate sub-routine

--- trimap_module.py
+++ trimap_module.py
@@ -1,14 +1,21 @@
 import cv2, os
 import numpy as np
 
-def trimap_generate(image, name, size, number):
+def trimap_generate(image, name, size, number, erosion=False):
     """
     This function creates a trimap based on simple dilation algorithm
-    Inputs [3]: a binary image (black & white only), name of the image, dilation pixels 
+    Inputs [4]: a binary image (black & white only), name of the image, dilation pixels 
+                the last argument is optional; i.e., how many iterations will the image get eroded                 
     Output    : a trimap
     """
-    pixels    = 2*size + 1;                                     # double and plus 1 to have an odd-sized kernel            
-    kernel    = np.ones((pixels,pixels),np.uint8)               # How many pixel of extension do I get
+    pixels    = 2*size + 1;                                     ## Double and plus 1 to have an odd-sized kernel            
+    kernel    = np.ones((pixels,pixels),np.uint8)               ## How many pixel of extension do I get
+
+    if erosion is not False:
+        erosion_kernel = np.ones((3,3), np.uint8)               ## Design an odd-sized erosion kernel
+        img = cv2.erode(img, erosion_kernel, iterations=3)      ## How many erosion do you expect
+        img = np.where(img > 0, 255, img)                       ## Any gray-clored pixel becomes white (smoothing)
+
     dilation  = cv2.dilate(image, kernel, iterations = 1)
 
     dilation  = np.where(dilation == 255, 127, dilation) 	## WHITE to GRAY
