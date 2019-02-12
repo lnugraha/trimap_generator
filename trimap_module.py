61795542c5b2a29f67de8836d57b638836dbdcf0
Leo <32232486+lnugraha@users.noreply.github.com>
2019-02-12 22:01:11 +0800

    Error handlers included
    (1.) Reject all non-binary images (all black, all white, grayscale, and RGB)
    (2.) Terminate the program if erosion has annihilated the entire foreground

--- trimap_module.py
+++ trimap_module.py
@@ -1,20 +1,33 @@
-import cv2, os
+import cv2, os, sys
 import numpy as np
 
 def check_image(image):
     """
-    To be completed:
     This function checks whether the input image is binary.
-    In other words, the following images will be rejected: grayscale, black only, white only, and RGB
+    To be completed: optimization in nested for-loop using Cython
     """
-    width  = image.shape[0];
-    height = image.shape[1];
-    
-    #for i in range(0,width):
-    #    for j in range (0,height):
-    if (image[0:,0:] == (0,0,0)): # if the image contains any value
-        print("ERROR: Non-Binary Image Detected")
-        return False
+    width  = image.shape[0]; height = image.shape[1];
+
+    # So far, input image is converted to grayscale;
+    # thus this is not an issue at the moment
+    if len(image.shape) > 2:
+        print("ERROR: non-binary image (RGB)");
+        sys.exit();
+
+    if cv2.countNonZero(image) == 0:
+        print("ERROR: non-binary image (all black)");
+        sys.exit();
+    else:
+        for i in range(0,width):
+            for j in range (0,height):
+                if (image[i,j] == 255 ):
+                    print("ERROR: non-binary (all white)");
+                    sys.exit();
+                elif (image[i,j] != 0 and image[i,j] != 255 ):
+                    print("ERROR: non-binary (grayscale)")
+                    sys.exit();
+                else:
+                    return True
 
 def trimap_generate(image, name, size, number, erosion=False):
     """
@@ -23,7 +36,6 @@ def trimap_generate(image, name, size, number, erosion=False):
                 the last argument is optional; i.e., how many iterations will the image get eroded                 
     Output    : a trimap
     """
-
     check_image(image);
 
     pixels    = 2*size + 1;                                     ## Double and plus 1 to have an odd-sized kernel            
@@ -32,8 +44,12 @@ def trimap_generate(image, name, size, number, erosion=False):
     if erosion is not False:
         erosion = int(erosion)                                    
         erosion_kernel = np.ones((3,3), np.uint8)                 ## Design an odd-sized erosion kernel
-        img = cv2.erode(img, erosion_kernel, iterations=erosion)  ## How many erosion do you expect
-        img = np.where(img > 0, 255, img)                         ## Any gray-clored pixel becomes white (smoothing)
+        image = cv2.erode(image, erosion_kernel, iterations=erosion)  ## How many erosion do you expect
+        image = np.where(image > 0, 255, image)                         ## Any gray-clored pixel becomes white (smoothing)
+        # Error-handler to prevent entire foreground annihilation
+        if cv2.countNonZero(image) == 0:
+            print("ERROR: foreground has been entirely eroded");
+            sys.exit();
 
     dilation  = cv2.dilate(image, kernel, iterations = 1)
 
@@ -48,3 +64,16 @@ def trimap_generate(image, name, size, number, erosion=False):
     path = "./images/results/"                                  ## Change the directory
     new_name = '{}px_'.format(size) + name + '_{}.png'.format(number);
     cv2.imwrite(os.path.join(path , new_name) , remake)
+
+""" Uncomment this part to test the modules above as Python script
+if __name__ == '__main__':
+    name  = "./images/test_images/test_image_0.png"; # ONLY test_image_0.png shall succeed
+    image = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+    size = 10;         
+    number = name[-5];
+    title = "test_image"
+
+    trimap_generate(image, title, size, number, erosion=10);
+    print("Here")
+"""
+
