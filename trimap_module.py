add5330cd340f5b4408b6cc626fa9cfb6945a771
Leo <32232486+lnugraha@users.noreply.github.com>
2019-03-03 16:09:40 +0800

    Update trimap_module.py

--- trimap_module.py
+++ trimap_module.py
@@ -1,9 +1,11 @@
 import cv2, os, sys
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
 
 def print_image(image,title,number):
     """
     Print your image to a text file
+    Checking purpose only
     """
     print(title+"_"+number);
     f= open("image.txt","w+")
@@ -12,7 +14,6 @@ def print_image(image,title,number):
             f.write("%d " %image[i,j])
         f.write("\n")
     f.close()
-
 
 def check_image(image):
     """
@@ -27,14 +28,14 @@ def check_image(image):
 
     TODO:
     1. Optimization in nested for-loop using Cython
-    2. Unsupervised Clustering - 
+    2. Unsupervised Clustering - find the largest possible chunk using KNN
     """
     # So far, input image is converted to grayscale;
     # thus this is not an issue at the moment
     if len(image.shape) > 2:
         print("ERROR: non-binary image (RGB)");
         sys.exit();
-    
+
     row = image.shape[0];
     col = image.shape[1];
 
@@ -55,7 +56,7 @@ def check_image(image):
         sys.exit();
     else:
         return True
-    
+
     """ REQUIRED REVISION - USE UNSUPERVISED LEARNING
     for i in range(0,row):
         for j in range (0,col):
@@ -70,17 +71,17 @@ def check_image(image):
 def trimap(image, name, size, number, erosion=False):
     """
     This function creates a trimap based on simple dilation algorithm
-    Inputs [4]: a binary image (black & white only), name of the image, dilation pixels 
-                the last argument is optional; i.e., how many iterations will the image get eroded                 
+    Inputs [4]: a binary image (black & white only), name of the image, dilation pixels
+                the last argument is optional; i.e., how many iterations will the image get eroded
     Output    : a trimap
     """
     check_image(image);
 
-    pixels    = 2*size + 1;                                     ## Double and plus 1 to have an odd-sized kernel            
+    pixels    = 2*size + 1;                                     ## Double and plus 1 to have an odd-sized kernel
     kernel    = np.ones((pixels,pixels),np.uint8)               ## How many pixel of extension do I get
 
     if erosion is not False:
-        erosion = int(erosion)                                    
+        erosion = int(erosion)
         erosion_kernel = np.ones((3,3), np.uint8)                 ## Design an odd-sized erosion kernel
         image = cv2.erode(image, erosion_kernel, iterations=erosion)  ## How many erosion do you expect
         image = np.where(image > 0, 255, image)                         ## Any gray-clored pixel becomes white (smoothing)
@@ -108,7 +109,7 @@ if __name__ == '__main__':
     path  = "./images/test_images/test_image_0.png";
 
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE);
-    size = 10;         
+    size = 10;
     number = path[-5];
     title = "test_image"
 
